1114c1ba1b0417703dd4e4f636385a881b5e1650
magnifiq <git.projectsss@gmail.com>
2023-11-03 20:39:27 +0100

    refactored and completelly changed project

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,25 @@
 const {src, dest, watch, series}=require('gulp') //we import here methods that we will use
 const sass=require('gulp-sass')(require('sass')) //we set here sass
 const purgecss=require('gulp-purgecss');
+const pug=require('gulp-pug');
+
+function pugCompile(){
+    return src('pugs/**/*.pug')
+        .pipe(pug())
+        .pipe(dest('pages'));
+}
 
 function buildStyles(){
     return src('styles/shinobi/**/*.scss')
     .pipe(sass())
-    .pipe(purgecss({content:['pages/*.html']}))
+    .pipe(purgecss({content:['pages/**/*.html']}))
     .pipe(dest('styles/css'))
 }
 
+
 function watchTask(){
-    watch(['styles/shinobi/**/*.scss', '*.html'], buildStyles)
+    watch(['pugs/**/*.pug'], pugCompile);
+    watch(['styles/shinobi/**/*.scss', '*.html'], buildStyles);
 }
 
-exports.default=series(buildStyles,watchTask);
+exports.default=series(pugCompile, buildStyles, watchTask);
