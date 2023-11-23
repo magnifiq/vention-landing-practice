3144165383d4518514b982b170e435e4c505b173
magnifiq <git.projectsss@gmail.com>
2023-11-23 23:10:52 +0100

    switch roles-fix in progress

--- js_part/changeTab.js
+++ js_part/changeTab.js
@@ -1,17 +1,24 @@
 const changeTab = (val, selectedClasses, activeClass, classesToHide) => {
   let tabs = document.querySelectorAll(selectedClasses);
+  let currRole;
   tabs.forEach((tab) => {
     tab.addEventListener("click", () => {
       tabs.forEach((el) => {
         el.classList.remove(activeClass);
       });
-       tab.classList.add(activeClass);
+      tab.classList.add(activeClass);
+      currRole = tab.innerHTML.trim();
+      console.log(currRole);
     });
   });
-  let sections = document.getElementsByClassName(classesToHide);
-  let sections_num = sections.length;
-  for (let i = 0; i < sections_num; i++) {
-    sections[i].style.display = "none";
+  if (classesToHide !== "homes") {
+    let sections = document.getElementsByClassName(classesToHide);
+    let sections_num = sections.length;
+    for (let i = 0; i < sections_num; i++) {
+      sections[i].style.display = "none";
+    }
+    document.getElementById(val).style.display = "block";
+  } else {
+    return currRole;
   }
-  document.getElementById(val).style.display = "block";
 };
