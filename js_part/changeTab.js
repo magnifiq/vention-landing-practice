38cc6dc84e1ece3797a7d41ad5848012416262c1
magnifiq <git.projectsss@gmail.com>
2023-11-28 13:16:23 +0100

    FIXED.congrats:)

--- js_part/changeTab.js
+++ js_part/changeTab.js
@@ -1,20 +1,14 @@
-const changeTab = (val, selectedClasses, activeClass, classesToHide) => {
+function changeTab(event, val, selectedClasses, activeClass, classesToHide) {
   let tabs = document.querySelectorAll(selectedClasses);
-  let currRole;
-///there is a problem
-  const clickHandler = (tab) => {
-    tabs.forEach((el) => {
-      el.classList.remove(activeClass);
-    });
-    tab.classList.add(activeClass);
-    currRole = tab.innerHTML.trim();
-    console.log(currRole);
-  };
 
-  tabs.forEach((tab) => {
-    tab.removeEventListener("click", () => clickHandler(tab));
-    tab.addEventListener("click", () => clickHandler(tab));
+  tabs.forEach((el) => {
+    el.classList.remove(activeClass);
   });
+  let clickedButton = event.currentTarget;
+  clickedButton.classList.add(activeClass);
+
+ 
+
   if (classesToHide !== "homes") {
     let sections = document.getElementsByClassName(classesToHide);
     let sections_num = sections.length;
@@ -22,8 +16,5 @@ const changeTab = (val, selectedClasses, activeClass, classesToHide) => {
       sections[i].style.display = "none";
     }
     document.getElementById(val).style.display = "block";
-  } else {
-    console.log(currRole);
   }
-  return tabs
-};
+}
