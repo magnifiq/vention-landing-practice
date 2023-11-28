799e7d79dc28edb2231c69f98da645f6cf92493a
magnifiq <git.projectsss@gmail.com>
2023-11-28 18:15:10 +0100

    somehow it works

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,22 @@
-const {src, dest, watch, series}=require('gulp') //we import here methods that we will use
-const sass=require('gulp-sass')(require('sass')) //we set here sass
-const purgecss=require('gulp-purgecss');
-const pug=require('gulp-pug');
+const { src, dest, watch, series } = require("gulp"); //we import here methods that we will use
+const sass = require("gulp-sass")(require("sass")); //we set here sass
+const purgecss = require("gulp-purgecss");
+const pug = require("gulp-pug");
 
-function pugCompile(){
-    return src('pugs/**/*.pug')
-        .pipe(pug())
-        .pipe(dest('pages'));
+function pugCompile() {
+  return src("src/pugs/**/*.pug").pipe(pug()).pipe(dest("dist/pages"));
 }
 
-function buildStyles(){
-    return src('styles/shinobi/**/*.scss')
+function buildStyles() {
+  return src("src/styles/**/*.scss")
     .pipe(sass())
-    .pipe(purgecss({content:['pages/**/*.html']}))
-    .pipe(dest('styles/css'))
+    .pipe(purgecss({ content: ["dist/pages/**/*.html"] }))
+    .pipe(dest("dist/styles/css"));
 }
 
-
-function watchTask(){
-    watch(['pugs/**/*.pug'], pugCompile);
-    watch(['styles/shinobi/**/*.scss', '*.html'], buildStyles);
+function watchTask() {
+  watch(["src/pugs/**/*.pug"], pugCompile);
+  watch(["src/styles/**/*.scss", "*.html"], buildStyles);
 }
 
-exports.default=series(pugCompile, buildStyles, watchTask);
+exports.default = series(pugCompile, buildStyles, watchTask);
